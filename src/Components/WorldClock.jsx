63f81a4995c23aaaeec18aa54788a2c60771c56a
Chandrashekar1004 <detectronbro@gmail.com>
2025-03-03 10:43:35 -0500

    Update WorldClock.jsx

--- src/Components/WorldClock.jsx
+++ src/Components/WorldClock.jsx
@@ -9,7 +9,7 @@ function WorldClock() {
     { id: 2, name: "England", zone: "Europe/London",url:"./src/assets/Flags/England.webp" },
     { id: 3, name: "USA", zone: "America/New_York",url:"./src/assets/Flags/USA.png" },
     { id: 4, name: "Canada", zone: "America/Toronto",url:"./src/assets/Flags/Canada.jpeg" },
-    { id: 5, name: "Australia", zone: "Australia/Sydney",url:"./src/assets/Flags/Australia.png" },
+    { id: 5, name: "Australia", zone: "src/assets/Flags/Australia.png" },
     { id: 6, name: "Russia", zone: "Europe/Moscow", url:"./src/assets/Flags/Russia.png"},
   ];
 
